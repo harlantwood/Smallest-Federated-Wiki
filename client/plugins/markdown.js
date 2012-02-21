9bf08f3456fd92ddd83e62ddb383c35cff98d758
Sven Schober <sschober@sssm.de>
2012-02-21 21:39:48 +0100

    be less intrusive
    inspired by plugins/line.coffee this change removes all mentions to
    `Markdown.Converter.js` or `Markdown.css` from static content and
    instead uses `wiki.getScript` to load them on demand.

--- client/plugins/markdown.js
+++ client/plugins/markdown.js
@@ -1,12 +1,15 @@
 (function() {
 
   window.plugins.markdown = {
-    emit: function(div, item) {
-      return div.append("<div class='markdown'>    " + (wiki.resolveLinks((new Markdown.Converter()).makeHtml(item.text))) + "</div>");
-    },
     bind: function(div, item) {
       return div.dblclick(function() {
         return wiki.textEditor(div, item);
+      });
+    },
+    emit: function(div, item) {
+      return wiki.getScript('/js/Markdown.Converter.js', function() {
+        div.append('<style>\n  .markdown ul {\n  margin: 0px;\n  list-style-position: inside;\n  padding:0px;\n}\n</style>');
+        return div.append("<div class='markdown'>      " + (wiki.resolveLinks((new Markdown.Converter()).makeHtml(item.text))) + "</div>");
       });
     }
   };
